00566260cf27c0a1cf9b1efe079954553ed79934
MerlyMalena <merlymalena2303@hotmail.com>
2026-01-19 17:09:30 -0400

    Ejercicio completado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    public class SettingsManager
+    {
+        private static SettingsManager? instancia; // El ? es para indicar que instancia puede estar null al inicio
+
+        public int volumen {get;set;}
+        public String resolucion {get;set;}
+
+        public SettingsManager()
+        {
+            volumen=0;
+            resolucion="HD";
+        }
+
+        public static SettingsManager ObtenerInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new SettingsManager();
+            }
+
+            return instancia;
+        }
+    }
+
+    static void Main(string[] args) //svm
+    {
+        var accesoMenu = SettingsManager.ObtenerInstancia();
+        accesoMenu.volumen=100;
+        
+        var accesoSonido = SettingsManager.ObtenerInstancia();
+        Console.WriteLine(accesoSonido.volumen);
+    }
+}
